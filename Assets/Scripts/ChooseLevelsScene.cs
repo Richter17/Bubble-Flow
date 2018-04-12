66c43f42a7df40fe4da7ff5698997c662576d2b9
ofer101 <ofer101@gmail.com>
2018-04-12 23:39:48 +0300

    Added Levels Scene

---
+++ Assets/Scripts/ChooseLevelsScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseLevelsScene : MonoBehaviour {
+
+    public string level;
+
+    public void OpenLevel()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
